da695bd06d6526f402cc2df90dee1be6559b7639
James <jcook208@student.cccs.edu>
2019-05-08 21:27:36 -0600

    funct. and to do list

--- Master_pi_Altered.py
+++ Master_pi_Altered.py
@@ -2,6 +2,7 @@ import RPi.GPIO as GPIO
 import smbus
 import time
 import numpy as np
+import cam_func as cf
 # for RPI version 1, use "bus = smbus.SMBus(0)"
 bus = smbus.SMBus(1)
 GPIO.setmode(GPIO.BOARD) #sets pin to sense when to transmit
@@ -25,23 +26,100 @@ def readNumber():
     # number = bus.read_byte_data(address, 1)
     return number
 
-while True and state:
-    
-    #if state:
-    data = open("Hi.txt" , "r")
-    lines = data.readlines()
-    lines = np.array(lines, dtype = np.int16)
-    for line in lines:
-        x = line
-        print(x)
-        bus.read_i2c_block_data(address, x)
-        #print ("RPI: Hi Arduino, I sent you a file")
-    data.close()
 
+def bb_1_telemtry:
+    while True and state:
         
-    # sleep one second
-    time.sleep(2)
+        #if state:
+        #data = cf.BB_1
+        data = open("Hi.txt", "r")
+        lines = data.readlines()
+        lines = np.array(lines, dtype = np.int16)
+    """
+###TO DO LIST PLANNING###
+create a detailed timeline of events for the systems
+save the timeline somewhere so we dont forget!
 
-    #number = readNumber()
-    #print ("Arduino: Hey RPI, I received a digit ", number)
-    #print
+###TO DO LIST BUS###
+integrate time functions into each telemtry function,
+**look into the hotline?**
+hookup different pi, to run camera
+test camera functions, make sure they all work seperately
+test all the timing
+
+###TO DO LIST ARMS###
+in between each data set, run arms code
+set all arms to relative zeros
+**check the servo system check, i.e. moving before called**
+
+    """
+        if elapsed_time >= 21:
+            for line in lines:
+                x = line
+                print(x)
+                bus.read_i2c_block_data(address, x)
+                #print ("RPI: Hi Arduino, I sent you a file")
+            data.close()
+
+            
+        # sleep one second
+        time.sleep(2)
+        break
+        
+
+def bb_2_telemtry:
+    while True and state:
+        
+        #if state:
+        #data = cf.BB_1
+        data = open("Hi.txt", "r")
+        lines = data.readlines()
+        lines = np.array(lines, dtype = np.int16)
+        for line in lines:
+            x = line
+            print(x)
+            bus.read_i2c_block_data(address, x)
+            #print ("RPI: Hi Arduino, I sent you a file")
+        data.close()
+
+            
+        # sleep one second
+        time.sleep(2)
+
+def bb_3_telemtry:
+    while True and state:
+        
+        #if state:
+        #data = cf.BB_1
+        data = open("Hi.txt", "r")
+        lines = data.readlines()
+        lines = np.array(lines, dtype = np.int16)
+        for line in lines:
+            x = line
+            print(x)
+            bus.read_i2c_block_data(address, x)
+            #print ("RPI: Hi Arduino, I sent you a file")
+        data.close()
+
+            
+        # sleep one second
+        time.sleep(2)
+
+def bb_4_telemtry:
+    while True and state:
+        
+        #if state:
+        #data = cf.BB_1
+        data = open("Hi.txt", "r")
+        lines = data.readlines()
+        lines = np.array(lines, dtype = np.int16)
+        for line in lines:
+            x = line
+            print(x)
+            bus.read_i2c_block_data(address, x)
+            #print ("RPI: Hi Arduino, I sent you a file")
+        data.close()
+
+            
+        # sleep one second
+        time.sleep(2)
