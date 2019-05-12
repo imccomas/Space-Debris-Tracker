918cfc0136cd49eb027a7cc7de11c8f6d46268aa
Ryan <35852086+ItsRyanLOL@users.noreply.github.com>
2019-05-12 01:33:52 -0600

    Added launcher code
    Added atleast some framework for launching our objects

--- Arduino Pro Mini Code/Serial_Bus_Code/Serial_Bus_Code.ino
+++ Arduino Pro Mini Code/Serial_Bus_Code/Serial_Bus_Code.ino
@@ -1,19 +1,34 @@
-#include <Wire.h>
+// Rocksat-x 2019 Control board software
+// ---
+// By: Ryan Wade
+// ---
+// This program controls CCofCO's rocksat-x payload, as well as telemeters data to the rocket
 
+#include <Wire.h>
+#include <Servo.h>
+
+#define LAUNCHER_HOME_POS 90 // Home posistion for servos (retaining ball)
+#define LAUNCHER_FINAL_POS 180 // Final posistion for launcher servo
+#define LAUNCHER_VELOCITY_DELAY 25 // Delay between angle changes on launchers to achive desired velocity (in ms)
+#define CHARGER_HOME_POS 90 // Home posistion for charging assembly servo
+#define CHARGER_ACTIVE_POS 180 // Charging posistion for motor servo
+#define CHARGE_POS 90 // Charger servo posistion for active charging
 #define SAMPLE_RATE 500 // Rate at which probes are read (in ms)
 #define TEENSY_ADR 0x66 // I2C address of Teensy board
+#define SLAVE_ADR 0x10 // Slave address used by arduino in slave mode
 #define BAUD_RATE 19200 // Rocket telemetry rate
+#define PROCESSING_TIME 5000 // Time delay before asking PIs for data after launch (in ms)
 
 //-- Pin definitions --
 #define START 2 // TE-1 signal line
-#define PWM_S1 3 // Servo 1 signal pin
+#define PWM_S0 3 // Servo 0 signal pin (motor servo)
 #define RBF 4 // Remove before flight pin
-#define PWM_S2 5 // Servo 2 signal pin
-#define PWM_S3 6 // Servo 3 signal pin
+#define PWM_S1 5 // Servo 1 signal pin
+#define PWM_S2 6 // Servo 2 signal pin
 #define PI2_TRIG 7 // PI Zero #2 I2C trigger line
 #define PI1_TRIG 8 // PI Zero #1 I2C trigger line
-#define PWM_S4 9 // Servo 4 signal line
-#define PWM_S5 10 // Servo 5 signal line
+#define PWM_S3 9 // Servo 3 signal line
+#define PWM_S4 10 // Servo 4 signal line
 #define LIGHTS 11 //  Light relay line
 #define SERVO4 12 // Servo 4 powerline control pin
 #define SERVO3 13 // Servo 3 powerline control pin
@@ -29,7 +44,11 @@ typedef union
   uint8_t bytes[4];
 } FLOATUNION_t;
 
-unsigned long lastTempSend;
+const int SERVOS[5] = { PWM_S0, PWM_S1, PWM_S2, PWM_S3, PWM_S4 }; 
+Servo myServos[5];
+uint8_t sequence = 0;
+unsigned long lastTempSend, timeCompleted = 0, nextEvent = 0;
+bool started = false;
 
 void setup() {
   Wire.begin();
@@ -46,7 +65,7 @@ void setup() {
   pinMode(PI2_TRIG, OUTPUT);
   pinMode(PI1_TRIG, OUTPUT);
   pinMode(PWM_S4, OUTPUT);
-  pinMode(PWM_S5, OUTPUT);
+  pinMode(PWM_S0, OUTPUT);
   pinMode(LIGHTS, OUTPUT);
   pinMode(SERVO4, OUTPUT);
   pinMode(SERVO3, OUTPUT);
@@ -68,27 +87,62 @@ void setup() {
   digitalWrite(MTR, LOW);
   // ---------------
 
+  // Attach Servos
+  myServos[0].attach(MTR_SRVO);
+  myServos[1].attach(PWM_S1);
+  myServos[2].attach(PWM_S2);
+  myServos[3].attach(PWM_S3);
+  myServos[4].attach(PWM_S4);
+  // Set all servos to home posistion
+  myServos[0].write(CHARGER_HOME_POS);
+  myServos[1].write(LAUNCHER_HOME_POS);
+  myServos[2].write(LAUNCHER_HOME_POS);
+  myServos[3].write(LAUNCHER_HOME_POS);
+  myServos[4].write(LAUNCHER_HOME_POS);
+  // ---------------
+
+
   lastTempSend = 0;
 }
 
 void loop() {
+  // Check if event timer has ended
+  if (nextEvent != 0 && nextEvent >= millis()) { // Time has elapsed for next event
 
-// ------------------
-// Experiment START code
-// ------------------
-  if (digitalRead(START) == HIGH) { // Experiment start condition met
+    switch (sequence) {
+      case 1: // Launch Control tube #1
+        launch(1);
+        nextEvent = millis() + PROCESSING_TIME; // Time (in ms) until next experiment event begins
+        ++sequence;
+        break;
+      case 2: // Read data from PIs
+        digitalWrite(LIGHTS, LOW); //Turn off chamber lighting
+        getPiData();
+        ++sequence;
+        break;
+    }
+  }
 
+  // ------------------
+  // Experiment START code
+  // ------------------
+  else if (!started && digitalRead(START) == HIGH) { // Experiment start condition met
+
+    started = true;
+
+    Serial.println(F("*************************"));
     Serial.print(F("START Command received at "));
     Serial.print(millis());
     Serial.println(F("ms."));
+    Serial.println(F("*************************"));
+    sequence = 1; // Set flag to start value
+    nextEvent = millis() + 1000; // Begin next event in 1 second
 
-    // Do your thing chicken wing
-    
   }
   // ---------------------
   //   TEMP PROBE READ/TELEMETRY OUT
   // --------------------
-  if (lastTempSend <= millis() + SAMPLE_RATE) {
+  else if (lastTempSend <= millis() + SAMPLE_RATE) {
 
     float probeData[5];
 
@@ -125,4 +179,84 @@ void sendTempTelem(float *temps) {
     Serial.print(temps[i]);
   }
   Serial.println();
+}
+
+// --------------------
+// Runs the sequence of commands required to launch a tube
+// --------------------
+void launch (uint8_t servo) {
+  
+  // Signal PIs that we are beginning the experiment,
+  // and they should begin recording for debris launch
+  digitalWrite(PI1_TRIG, HIGH);
+  digitalWrite(PI2_TRIG, HIGH);
+  delay(20);
+  digitalWrite(PI1_TRIG, LOW);
+  digitalWrite(PI2_TRIG, LOW);
+
+  digitalWrite(LIGHTS, HIGH); // Turn on chamber lights
+  myServos[servo].write(LAUNCHER_HOME_POS); // Make sure servo wants to travel to home posistion
+  digitalWrite(SERVOS[servo], HIGH); // Arm launcher servo
+
+  for (int i = LAUNCHER_HOME_POS; i < LAUNCHER_FINAL_POS; ++i) {
+    myServos[servo].write(i);
+    delay(LAUNCHER_VELOCITY_DELAY);
+  }
+  
+  
+
+  //INSERT: Code to move servo at set rate.
+
+}
+
+void beginCharging() {
+  digitalWrite(MTR, HIGH); // Turn on motor
+  digitalWrite(MTR_SRVO, HIGH); // Turn on power to motor servo
+  // INSERT: Move servo to charge posistion
+}
+
+void endCharging() {
+
+  // INSERT: Move servo to home posistion
+}
+
+// --------------------
+// Pulls the data from both Raspberry Pi Zeros by switching to I2C slave mode, and signaling
+// the Pi that the Arduino is ready to receive data
+// --------------------
+void getPiData() {
+
+  Wire.begin(SLAVE_ADR);
+
+  digitalWrite(PI1_TRIG, HIGH); // Tell Pi #1 we are ready for data
+
+  while (!Wire.available()) { // Wait for data in buffer
+    delay(5);
+  }
+  Serial.println(F("******** BEGIN PI #1 DATA ********"));
+
+  while (Wire.available()) { // Read data in buffer
+    Serial.print(Wire.read()); // Move buffer data directly to telem line
+  }
+  Serial.println();
+  Serial.println(F("******** END PI #1 DATA ********"));
+
+  digitalWrite(PI1_TRIG, LOW); // Bring trigger line back low
+
+  digitalWrite(PI2_TRIG, HIGH); // Tell Pi #2 we are ready for data
+
+  while (!Wire.available()) { // Wait for data in buffer
+    delay(5);
+  }
+  Serial.println(F("******** BEGIN PI #2 DATA ********"));
+
+  while (Wire.available()) { // Read data in buffer
+    Serial.print(Wire.read()); // Move buffer data directly to telem line
+  }
+  Serial.println();
+  Serial.println(F("******** END PI #2 DATA ********"));
+
+  digitalWrite(PI2_TRIG, LOW); // Bring trigger line back low
+
+  Wire.begin(); // Return to master mode
 }
