ea48fe27a85ba61511801250a113902e88a72cea
Ryan <35852086+ItsRyanLOL@users.noreply.github.com>
2019-05-20 03:43:08 -0600

    more

--- Arduino Pro Mini Code/Pro_mini_code/Pro_mini_code.ino
+++ Arduino Pro Mini Code/Pro_mini_code/Pro_mini_code.ino
@@ -110,7 +110,7 @@ void setup() {
 
   if (digitalRead(RBF) == HIGH) { // Remove Beofre Flight pin is present at power on
     rbf = false; 
-    Serial.println(("-- RBF Inhibitor PRESENT --"));
+    Serial.println(F("-- RBF Inhibitor PRESENT at POWERON --"));
   }
 
   lastTempSend = 0;
